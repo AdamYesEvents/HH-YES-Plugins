efeac138d798f7b04a32ae036d2fecf83e866a55
Adam Nicholls <Adam@yesevents.co.uk>
2026-06-25 17:09:48 +0100

    Loader v0.1.8: pin stage-designer to v0.5.0

--- loader.js
+++ loader.js
@@ -7,9 +7,9 @@
  *
  * Load via jsDelivr (NOT raw.githubusercontent.com, which serves text/plain+nosniff
  * and will not execute). Pin the loader to a release tag, e.g.:
- *   https://cdn.jsdelivr.net/gh/AdamYesEvents/HH-YES-Plugins@v0.1.7/loader.js
+ *   https://cdn.jsdelivr.net/gh/AdamYesEvents/HH-YES-Plugins@v0.1.8/loader.js
  *
- * Version: 0.1.7
+ * Version: 0.1.8
  */
 
 (function () {
@@ -25,7 +25,7 @@
   // create a tag and set ref to it here.
   // ===========================================================================
   var TOOLS = {
-    "stage-designer":    { on: true, ref: "stage-designer-v0.4.0" },
+    "stage-designer":    { on: true, ref: "stage-designer-v0.5.0" },
     "videowall-creator": { on: true, ref: "videowall-creator-v0.1.0" }
   };
 
