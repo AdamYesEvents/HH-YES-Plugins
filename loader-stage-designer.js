14d771517f805ed0e8b51e1bf83840319bf4ddcd
Adam Nicholls <Adam@yesevents.co.uk>
2026-07-31 09:44:10 +0100

    Loader v0.1.54: pin tool to new commit sha with v0.28.0 (auto-logo)

--- loader-stage-designer.js
+++ loader-stage-designer.js
@@ -7,7 +7,7 @@
  * Install via jsDelivr:
  *   https://cdn.jsdelivr.net/gh/AdamYesEvents/HH-YES-Plugins@v0.1.52/loader-stage-designer.js
  *
- * Version: 0.1.52
+ * Version: 0.1.53
  */
 
 (function () {
@@ -19,7 +19,7 @@
   // decoupled from the loader version.
   // ===========================================================================
   var TOOLS = {
-    "stage-designer": { on: true, ref: "72ed5f2" }
+    "stage-designer": { on: true, ref: "8eff435" }
   };
 
   // ---------------------------------------------------------------------------
