61de1397afbe4633d6cd07bba63344b42d458794
Adam Nicholls <adam@yesevents.co.uk>
2026-08-11 15:54:48 +0100

    loader v0.1.78: pin videowall-creator to v0.8.0 (processor Q5 + LSU qty fix)

--- loader.js
+++ loader.js
@@ -7,9 +7,9 @@
  *
  * Load via jsDelivr (NOT raw.githubusercontent.com, which serves text/plain+nosniff
  * and will not execute). Pin the loader to a release tag, e.g.:
- *   https://cdn.jsdelivr.net/gh/AdamYesEvents/HH-YES-Plugins@v0.1.77/loader.js
+ *   https://cdn.jsdelivr.net/gh/AdamYesEvents/HH-YES-Plugins@v0.1.78/loader.js
  *
- * Version: 0.1.77
+ * Version: 0.1.78
  */
 
 (function () {
@@ -26,7 +26,7 @@
   // ===========================================================================
   var TOOLS = {
     "stage-designer":    { on: true, ref: "7913358c9f217c109f75ae76de0dfeaf461e0304" },
-    "videowall-creator": { on: true, ref: "videowall-creator-v0.7.0" }
+    "videowall-creator": { on: true, ref: "videowall-creator-v0.8.0" }
   };
 
   // ---------------------------------------------------------------------------
