fee3fab8becfbb429f1c36ea3dad35e17651f33d
Adam Nicholls <Adam@yesevents.co.uk>
2026-08-13 13:39:01 +0100

    Loader v0.1.87: pin stage-designer to v0.31.11 (all-customs + progress bar + 2s throttle)

--- loader-stage-designer.js
+++ loader-stage-designer.js
@@ -5,9 +5,9 @@
  * tool. Full loader (both tools) is at loader.js.
  *
  * Install via jsDelivr:
- *   https://cdn.jsdelivr.net/gh/AdamYesEvents/HH-YES-Plugins@v0.1.86/loader-stage-designer.js
+ *   https://cdn.jsdelivr.net/gh/AdamYesEvents/HH-YES-Plugins@v0.1.87/loader-stage-designer.js
  *
- * Version: 0.1.86
+ * Version: 0.1.87
  */
 
 (function () {
@@ -19,7 +19,7 @@
   // decoupled from the loader version.
   // ===========================================================================
   var TOOLS = {
-    "stage-designer": { on: true, ref: "89bea32f5afdbd3f1425948ce7dcd37fe67f5c9f" }
+    "stage-designer": { on: true, ref: "289a6461e20ae2db68a07cfaa3577fc46676af99" }
   };
 
   // ---------------------------------------------------------------------------
--- loader.js
+++ loader.js
@@ -7,9 +7,9 @@
  *
  * Load via jsDelivr (NOT raw.githubusercontent.com, which serves text/plain+nosniff
  * and will not execute). Pin the loader to a release tag, e.g.:
- *   https://cdn.jsdelivr.net/gh/AdamYesEvents/HH-YES-Plugins@v0.1.86/loader.js
+ *   https://cdn.jsdelivr.net/gh/AdamYesEvents/HH-YES-Plugins@v0.1.87/loader.js
  *
- * Version: 0.1.86
+ * Version: 0.1.87
  */
 
 (function () {
@@ -25,7 +25,7 @@
   // create a tag and set ref to it here.
   // ===========================================================================
   var TOOLS = {
-    "stage-designer":    { on: true, ref: "89bea32f5afdbd3f1425948ce7dcd37fe67f5c9f" },
+    "stage-designer":    { on: true, ref: "289a6461e20ae2db68a07cfaa3577fc46676af99" },
     "videowall-creator": { on: true, ref: "videowall-creator-v0.8.0" }
   };
 
